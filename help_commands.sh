843727396511f28865daaa64629ed82aa2c427e3
BlueBixt <newssungoldentoday@gmail.com>
2026-07-06 17:23:12 +0800

    Update help_commands.sh

--- help_commands.sh
+++ help_commands.sh
@@ -5,8 +5,14 @@
 Ray Chad Help Commands 
 
 ===========================================
+
+Ray Chad Help Command to Users to help 
+and Understanding the Commands from the Ray 
+Chad
+
 ===========================================
 
 clear
+
 
 
