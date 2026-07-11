440f7d607840deacd5664712bbb3b4d05448978a
BlueBixt <newssungoldentoday@gmail.com>
2026-07-12 07:19:49 +0800

    Update Domino.nix

--- envs/Domino.nix
+++ envs/Domino.nix
@@ -9,10 +9,10 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ bash curl ];
   dontBuild = true;
   installPhase = ''
-    mkdir -p $out/etc/raychd/envs/dacko $out/bin
-    echo "DOMINO_ENABLED=true" > $out/etc/raychd/envs/dacko/.env
-    echo "DOMINO_VERSION=1.0.0" >> $out/etc/raychd/envs/dacko/.env
-    echo "DOMINO_DAEMON_MODE=true" >> $out/etc/raychd/envs/dacko/.env
+    mkdir -p $out/etc/raychd/envs/domino $out/bin
+    echo "DOMINO_ENABLED=true" > $out/etc/raychd/envs/domino/.env
+    echo "DOMINO_VERSION=1.0.0" >> $out/etc/raychd/envs/domino/.env
+    echo "DOMINO_DAEMON_MODE=true" >> $out/etc/raychd/envs/domino/.env
   '';
   meta = with lib; { description = "Daemon controller environment for Ray Chad"; homepage = "https://github.com/newssungoldentoday-dev/raychd"; license = licenses.asl20; platforms = platforms.unix; };
 }
