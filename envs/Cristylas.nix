4d27d1bef271ab282af3503adff526e35ddca004
BlueBixt <newssungoldentoday@gmail.com>
2026-07-12 07:18:20 +0800

    Create Cristylas.nix

---
+++ envs/Cristylas.nix
@@ -0,0 +1,18 @@
+{ lib, stdenv, fetchurl, bash, curl }:
+stdenv.mkDerivation rec {
+  pname   = "raychd-env-cristylas";
+  version = "1.0.0";
+  src = fetchurl {
+    url    = "https://github.com/newssungoldentoday-dev/raychd/archive/refs/heads/main.tar.gz";
+    sha256 = lib.fakeSha256;
+  };
+  nativeBuildInputs = [ bash curl ];
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/etc/raychd/envs/cristylas $out/bin
+    echo "CRISTYLAS_ENABLED=true" > $out/etc/raychd/envs/cristylas/.env
+    echo "CRISTYLAS_VERSION=1.0.0" >> $out/etc/raychd/envs/cristylas/.env
+    echo "CRISTYLAS_DAEMON_MODE=true" >> $out/etc/raychd/envs/cristylas/.env
+  '';
+  meta = with lib; { description = "Daemon controller environment for Ray Chad"; homepage = "https://github.com/newssungoldentoday-dev/raychd"; license = licenses.asl20; platforms = platforms.unix; };
+}
